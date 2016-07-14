47b9fc70f328a7c426cdeb5f9ea96b8d3df272ef
Che-Lun Chang <evnivekol@gmail.com>
2016-07-13 22:32:10 -0700

    CC - Changes make in Class2 project

---
+++ Assets/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody _CaschedRB;
+    private Vector3 _Move;
+
+    public void Start()
+    {
+        _CaschedRB = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 move)
+    {
+        _Move += move;
+    }
+
+    public void FixedUpdate()
+    {
+        _CaschedRB.AddForce(_Move);
+
+        _Move = Vector3.zero;
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+    [Header("Player object values")]
+    public Ball ball;
+    [Tooltip("Speed in units per second")]
+    [Range(1,144f)]
+    public float speed;
+
+    public void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        ball.Move(new Vector3(h, 0f, v));
+
+    }
+}
